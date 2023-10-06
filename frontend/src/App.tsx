5215d7a4b5d79fc5ea8db0c927762b76113b62d0
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-06 12:50:20 +0200

    added view for book list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,19 @@
-import './App.css'
+import './App.css';
 import {useEffect, useState} from "react";
 import {Book} from "./Types.tsx";
 import axios from "axios";
+import BookList from "./components/BookList.tsx";
 
 export default function App() {
-    const [books, setBooks] = useState<Book[]>([])
+    const [books, setBooks] = useState<Book[]>([]);
 
-    useEffect(()=>{
-
-
-    }, [])
+    useEffect(loadAllBooks, []);
 
     function loadAllBooks (){
         axios.get("/api/books")
             .then((response) => {
+                if (response.status!==200)
+                    throw "Get wrong response status, when loading all books: "+response.status;
                 setBooks(response.data);
             })
             .catch((error)=>{
@@ -24,8 +24,8 @@ export default function App() {
 
     return (
         <>
-
-            <p>Hello</p>
+            <h1>Book Library</h1>
+            <BookList books={books}/>
         </>
     )
 }
---
+++ frontend/src/components/BookCard.tsx
@@ -0,0 +1,16 @@
+import {Book} from "../Types.tsx";
+
+type Props = {
+    book: Book
+}
+
+export default function BookCard( props: Props ) {
+
+    return (
+        <div>
+            <div>id     : {props.book.id     }</div>
+            <div>title  : {props.book.title  }</div>
+            <div>author : {props.book.author }</div>
+        </div>
+    )
+}

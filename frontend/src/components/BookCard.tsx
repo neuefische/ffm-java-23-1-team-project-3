56a66ae2d99731f322d954a64f52f74d7a1a81f7
neuefische <info@neuefische.de>
2023-10-09 13:01:56 +0200

    add deleteCard function

--- frontend/src/components/BookCard.tsx
+++ frontend/src/components/BookCard.tsx
@@ -1,16 +1,24 @@
 import {Book} from "../Types.tsx";
+import axios from "axios";
 
 type Props = {
     book: Book
+    onItemChange: () => void
 }
 
 export default function BookCard( props: Props ) {
+
+    function deleteCard() {
+        axios.delete("/api/books/" + props.book.id)
+            .then(props.onItemChange)
+    }
 
     return (
         <div className="BookCard">
             <div>id     : {props.book.id     }</div>
             <div>title  : {props.book.title  }</div>
             <div>author : {props.book.author }</div>
+            <button onClick={deleteCard}>X</button>
         </div>
     )
 }

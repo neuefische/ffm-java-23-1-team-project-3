2d62657242a553dfa088e935d9c4a411c3fd4fc2
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-09 16:03:05 +0200

    feat: added 2nd case of integration test for  GetAllBooks

--- backend/src/test/java/de/neuefische/backend/LibraryIntegrationTest.java
+++ backend/src/test/java/de/neuefische/backend/LibraryIntegrationTest.java
@@ -18,6 +18,9 @@ public class LibraryIntegrationTest {
 	@Autowired
 	private MockMvc mockMvc;
 
+	@Autowired
+	private LibraryRepository libraryRepository;
+
 	@Test
 	@DirtiesContext
 	void whenGetAllBooks_performsOnEmptyRepo_returnsEmptyJsonArray() throws Exception {
@@ -32,6 +35,33 @@ public class LibraryIntegrationTest {
 				// Then
 				.andExpect(status().isOk())
 				.andExpect(content().json("[]"));
+	}
+
+	@Test
+	@DirtiesContext
+	void whenGetAllBooks_performsOnFilledRepo_returnsRepoContent() throws Exception {
+		// Given
+		libraryRepository.save(new Book("id1", "Title 1", "Author 1"));
+		libraryRepository.save(new Book("id2", "Title 2", "Author 2"));
+		libraryRepository.save(new Book("id3", "Title 3", "Author 3"));
+		libraryRepository.save(new Book("id4", "Title 4", "Author 4"));
+
+		// When
+		mockMvc
+				.perform(MockMvcRequestBuilders
+						.get("/api/books")
+				)
+
+				// Then
+				.andExpect(status().isOk())
+				.andExpect(content().json("""
+					[
+						{ "id": "id1", "title": "Title 1", "author": "Author 1" },
+						{ "id": "id2", "title": "Title 2", "author": "Author 2" },
+						{ "id": "id3", "title": "Title 3", "author": "Author 3" },
+						{ "id": "id4", "title": "Title 4", "author": "Author 4" }
+					]
+				"""));
 	}
 
 

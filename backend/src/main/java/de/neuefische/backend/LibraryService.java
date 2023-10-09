32acbde35b74c63ce74c2091607c3d53631f530f
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-09 12:58:42 +0200

    feat: [Edit] added unit test of updateBook in LibraryService
    added an id parameter and some checks that will be needed in Controller

--- backend/src/main/java/de/neuefische/backend/LibraryService.java
+++ backend/src/main/java/de/neuefische/backend/LibraryService.java
@@ -1,9 +1,12 @@
 package de.neuefische.backend;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +17,14 @@ public class LibraryService {
         return libraryRepository.findAll();
     }
 
-    public Book update(Book book) {
+    public Book updateBook(@NonNull String id, @NonNull Book book) {
+        if (!id.equals(book.id()))
+            throw new IllegalArgumentException("updateBook( id:%s, book:{ id:%s } ) -> given Id and Id of book are different".formatted(id, book.id()));
+
+        Optional<Book> existingBook = libraryRepository.findById(id);
+        if (existingBook.isEmpty())
+            throw new NoSuchElementException("updateBook( id:%s, book:{ id:%s } ) -> Can't find a book with Id \"%s\"".formatted(id, book.id(), book.id()));
+
         return libraryRepository.save(book);
     }
 }
--- backend/src/test/java/de/neuefische/backend/LibraryServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/LibraryServiceTest.java
@@ -2,8 +2,11 @@ package de.neuefische.backend;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -58,6 +61,54 @@ class LibraryServiceTest {
 				new Book("127","Title5","Author5"),
 				new Book("128","Title6","Author6")
 		);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void whenUpdateProduct_getsInvalidIDs_throwException() {
+		// Given
+		String id = "456";
+		String bookId = "457";
+
+		// When
+		Executable executable = () -> libraryService.updateBook(id, new Book(bookId, "Title 1", "Author 1"));
+
+		// Then
+		assertThrows(IllegalArgumentException.class, executable);
+	}
+
+	@Test
+	void whenUpdateProduct_getsUnknownID_throwException() {
+		// Given
+		when(libraryRepository.findById("456")).thenReturn(
+				Optional.empty()
+		);
+
+		// When
+		Executable executable = () -> libraryService.updateBook("456", new Book("456", "Title 1", "Author 1"));
+
+		// Then
+		assertThrows(NoSuchElementException.class, executable);
+		verify(libraryRepository).findById("456");
+	}
+
+	@Test
+	void whenUpdateProduct_getsValidID_returnsChangedBook() {
+		// Given
+		when(libraryRepository.findById("456")).thenReturn(
+				Optional.of(new Book("456", "Title A", "Author A"))
+		);
+		when(libraryRepository.save(new Book("456", "Title 1", "Author 1"))).thenReturn(
+				new Book("456", "Title 1", "Author 1")
+		);
+
+		// When
+		Book actual = libraryService.updateBook("456", new Book("456", "Title 1", "Author 1"));
+
+		// Then
+		verify(libraryRepository).findById("456");
+		verify(libraryRepository).save(new Book("456", "Title 1", "Author 1"));
+		Book expected = new Book("456", "Title 1", "Author 1");
 		assertEquals(expected, actual);
 	}
 

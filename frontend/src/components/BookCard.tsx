8f130458160978d81404951d00cb5c74d34cce5f
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-09 11:49:56 +0200

    feat: [Edit] added frontend

--- frontend/src/components/BookCard.tsx
+++ frontend/src/components/BookCard.tsx
@@ -1,16 +1,19 @@
 import {Book} from "../Types.tsx";
+import {useNavigate} from "react-router-dom";
 
 type Props = {
     book: Book
 }
 
 export default function BookCard( props: Props ) {
+    const navigate = useNavigate();
 
     return (
         <div className="BookCard">
             <div>id     : {props.book.id     }</div>
             <div>title  : {props.book.title  }</div>
             <div>author : {props.book.author }</div>
+            <button onClick={() => navigate("/books/"+props.book.id+"/edit")}>Edit</button>
         </div>
     )
 }
---
+++ frontend/src/components/EditBook.tsx
@@ -0,0 +1,84 @@
+import {Book} from "../Types.tsx";
+import {useNavigate, useParams} from "react-router-dom";
+import {ChangeEvent, FormEvent, useEffect, useState} from "react";
+import axios from "axios";
+
+type Props = {
+    books: Book[]
+    reload: ()=>void
+}
+
+export default function EditBook(props: Props ) {
+    const { id } = useParams()
+    const navigate = useNavigate();
+    console.debug(`Rendering EditBookForm { id:"${id}", props.books: ${props.books.length} books }`)
+
+    const filteredBooks:Book[] = props.books.filter(e => e.id === id)
+
+    if (filteredBooks.length < 1)
+        return <>
+            Can't find book with id "{id}"<br/>
+            <button type="button" onClick={() => navigate("/")}>Back</button>
+        </>
+
+    return <EditBookForm book={filteredBooks[0]} reload={props.reload}/>
+}
+
+type FormProps = {
+    book: Book
+    reload: ()=>void
+}
+
+function EditBookForm( props: FormProps ) {
+    const [book, updateBook] = useState<Book>(props.book)
+    useEffect(
+        ()=> updateBook(props.book),
+        [ props.book ]
+    )
+    const navigate = useNavigate();
+    console.debug(`Rendering EditBookForm { id:"${props.book.id}" }`)
+
+    function updateBookValue( name:string, value:string ) {
+        updateBook( {
+            ...book,
+            [name]: value
+        } )
+    }
+
+    function onChangeFcnI( event: ChangeEvent<HTMLInputElement> ) {
+        updateBookValue( event.target.name, event.target.value )
+    }
+
+    function saveChanges( event: FormEvent<HTMLFormElement> ) {
+        event.preventDefault()
+        update(book)
+        navigate("/")
+    }
+
+    function update( book: Book ) {
+        axios
+            .put('/api/books/'+book.id, book )
+            .then(response => {
+                if (response.status != 200)
+                    throw {error: "Got wrong status on update book: " + response.status}
+                props.reload()
+            })
+            .catch(reason => {
+                console.error(reason)
+            })
+    }
+
+    return (
+        <>
+            <form className="EditBookForm" onSubmit={saveChanges}>
+                <label>id     : {book.id}</label>
+                <label>title  : <input name="title"  value={book.title } onChange={onChangeFcnI}/></label>
+                <label>author : <input name="author" value={book.author} onChange={onChangeFcnI}/></label>
+                <div>
+                    <button>Save</button>
+                    <button type="button" onClick={() => navigate("/")}>Cancel</button>
+                </div>
+            </form>
+        </>
+    )
+}

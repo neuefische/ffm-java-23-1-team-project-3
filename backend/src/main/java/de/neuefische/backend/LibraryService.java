5211a8dec230c0a3865066ba73e295b95b867227
dianabreik <diana.sam341@gmail.com>
2023-10-10 12:18:05 +0200

    delete what is not important

--- backend/src/main/java/de/neuefische/backend/LibraryService.java
+++ backend/src/main/java/de/neuefische/backend/LibraryService.java
@@ -1,10 +1,7 @@
 package de.neuefische.backend;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 import java.util.NoSuchElementException;

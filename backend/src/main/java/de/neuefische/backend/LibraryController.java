a1c1f0de2e0febbaed1e1cc25cafb7be5c79d2ce
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-11 20:27:12 +0200

    fix: fixed sonar lint warning about System.err.print in LibraryController

--- backend/src/main/java/de/neuefische/backend/LibraryController.java
+++ backend/src/main/java/de/neuefische/backend/LibraryController.java
@@ -1,6 +1,7 @@
 package de.neuefische.backend;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +10,7 @@ import java.util.NoSuchElementException;
 @RestController
 @RequestMapping("/api/books")
 @RequiredArgsConstructor
+@Slf4j
 public class LibraryController {
 
     private final LibraryService libraryService;
@@ -46,15 +48,17 @@ public class LibraryController {
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorMessage handleIllegalArgumentException(IllegalArgumentException ex) {
-        System.err.printf("IllegalArgumentException: %s%n", ex.getMessage());
-        return new ErrorMessage("IllegalArgumentException: %s".formatted(ex.getMessage()));
+        String messsage = "IllegalArgumentException: %s".formatted(ex.getMessage());
+        log.error(messsage);
+        return new ErrorMessage(messsage);
     }
 
     @ExceptionHandler(NoSuchElementException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorMessage handleNoSuchElementException(NoSuchElementException ex) {
-        System.err.printf("NoSuchElementException: %s%n", ex.getMessage());
-        return new ErrorMessage("NoSuchElementException: %s".formatted(ex.getMessage()));
+        String message = "NoSuchElementException: %s".formatted(ex.getMessage());
+        log.error(message);
+        return new ErrorMessage(message);
     }
 
 }

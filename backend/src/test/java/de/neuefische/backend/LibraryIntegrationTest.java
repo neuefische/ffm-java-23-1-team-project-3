adb50a5eab1b66539875b977edf7d01d44d555e9
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-11 19:41:11 +0200

    fix: fixed LibraryServiceTest & LibraryIntegrationTest
    sorry, something forced me to replace code in LibraryServiceTest.deleteBook :/

--- backend/src/test/java/de/neuefische/backend/LibraryIntegrationTest.java
+++ backend/src/test/java/de/neuefische/backend/LibraryIntegrationTest.java
@@ -19,11 +19,17 @@ class LibraryIntegrationTest {
 
 	@Autowired
 	private LibraryRepository libraryRepository;
+	@Autowired
+	private TimestampRepository timestampRepository;
+
 
 	@Test
 	@DirtiesContext
 	void whenGetAllBooks_performsOnEmptyRepo_returnsEmptyJsonArray() throws Exception {
 		// Given
+		Timestamp now = new Timestamp("test", "<TestTimestamp>");
+		timestampRepository.save(now);
+
 		// When
 		mockMvc
 				.perform(MockMvcRequestBuilders
@@ -32,7 +38,15 @@ class LibraryIntegrationTest {
 
 				// Then
 				.andExpect(status().isOk())
-				.andExpect(content().json("[]"));
+				.andExpect(content().json("""
+					{
+						"books": [],
+						"timestamp": {
+							"id": "test",
+							"timestamp": "<TestTimestamp>"
+						}
+					}
+				"""));
 	}
 
     @Test
@@ -42,19 +56,19 @@ class LibraryIntegrationTest {
         String id= "1";
         Book book = new Book(id,"title 1","author 1");
         libraryRepository.save(book);
+
         //WHEN
         mockMvc.perform(MockMvcRequestBuilders.get("/api/books/"+ id))
+
                 //THEN
                 .andExpect(status().isOk())
                 .andExpect(content().json("""
-                          {
-                          "id": "1",
-                          "title": "title 1",
-                          "author": "author 1"
-                          }
-
-                        
-"""));
+					{
+						"id": "1",
+						"title": "title 1",
+						"author": "author 1"
+					}
+				"""));
     }
 
     @Test
@@ -62,8 +76,10 @@ class LibraryIntegrationTest {
     void getBookByID_ifNotFound_handleNoSuchElementException() throws Exception {
         //GIVEN
         String id= "3";
+
         //WHEN
         mockMvc.perform(MockMvcRequestBuilders.get("/api/books/"+ id))
+
                 //THEN
                 .andExpect(status().isNotFound());
     }
@@ -76,20 +92,30 @@ class LibraryIntegrationTest {
 		libraryRepository.save(new Book("id2", "Title 2", "Author 2"));
 		libraryRepository.save(new Book("id3", "Title 3", "Author 3"));
 		libraryRepository.save(new Book("id4", "Title 4", "Author 4"));
+		Timestamp now = new Timestamp("test", "<TestTimestamp>");
+		timestampRepository.save(now);
+
 		// When
 		mockMvc
 				.perform(MockMvcRequestBuilders
 						.get("/api/books")
 				)
+
 				// Then
 				.andExpect(status().isOk())
 				.andExpect(content().json("""
-					[
-						{ "id": "id1", "title": "Title 1", "author": "Author 1" },
-						{ "id": "id2", "title": "Title 2", "author": "Author 2" },
-						{ "id": "id3", "title": "Title 3", "author": "Author 3" },
-						{ "id": "id4", "title": "Title 4", "author": "Author 4" }
-					]
+					{
+						"books": [
+							{ "id": "id1", "title": "Title 1", "author": "Author 1" },
+							{ "id": "id2", "title": "Title 2", "author": "Author 2" },
+							{ "id": "id3", "title": "Title 3", "author": "Author 3" },
+							{ "id": "id4", "title": "Title 4", "author": "Author 4" }
+						],
+						"timestamp": {
+							"id": "test",
+							"timestamp": "<TestTimestamp>"
+						}
+					}
 				"""));
 	}
 
@@ -98,8 +124,10 @@ class LibraryIntegrationTest {
 	void removeBookTest() throws Exception {
 		// Given
 		libraryRepository.save(new Book("1", "My new book", "Me"));
+
 		// When
 		mockMvc.perform(MockMvcRequestBuilders.delete("/api/books/1"))
+
 				// Then
 				.andExpect(status().isOk());
 	}
@@ -108,16 +136,22 @@ class LibraryIntegrationTest {
     @DirtiesContext
     void whenAddBooks_getsNewBooks_returnsBooks() throws Exception {
         // Given
+
         // When
         mockMvc
                 .perform(MockMvcRequestBuilders
                         .post("/api/books")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{ \"title\":\"Title 1\", \"author\": \"Author1\"}")
+                        .content("""
+							{ "title": "Title 1", "author": "Author1" }
+						""")
                 )
+
                 // Then
                 .andExpect(status().isCreated())
-                .andExpect(content().json("{ \"title\":\"Title 1\", \"author\": \"Author1\" }"))
+                .andExpect(content().json("""
+					{ "title": "Title 1", "author": "Author1" }
+				"""))
                 .andExpect(jsonPath("$.id").isString());
     }
 
@@ -189,7 +223,7 @@ class LibraryIntegrationTest {
 				// Then
 				.andExpect(status().isOk())
 				.andExpect(content().json("""
-							{ "id": "id1", "title": "Title 1B", "author": "Author 1B" }
+					{ "id": "id1", "title": "Title 1B", "author": "Author 1B" }
 				"""));
 	}
 }

ca137c12385b86c6596ed4018c1784bb2cbfa081
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-12 12:15:38 +0200

    feat: [MoreDetails] added new fields to types in frontend and backend

--- frontend/src/Types.tsx
+++ frontend/src/Types.tsx
@@ -1,6 +1,10 @@
 
 export type Book = {
-    id: string,
-    title: string,
+    id: string
+    title: string
     author: string
+    description: string
+    publisher: string
+    isbn: string
+    coverUrl: string
 }

d03f4409dad05a5213412eb450598fd0303eeacc
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-13 12:14:11 +0200

    refactor: [CoverUpload] cover selection is working
    selected cover will be shown in form as preview

--- frontend/src/components/AddEditBookForm.tsx
+++ frontend/src/components/AddEditBookForm.tsx
@@ -8,10 +8,23 @@ type Props = {
     saveButtonTitle: string
 }
 
+const defaultCoverSourceType: string = "url";
+
 export default function AddEditBookForm( props: Props ) {
     const [book, updateBook] = useState<Book>(props.book);
+    const [displayedCoverImage, setDisplayedCoverImage] = useState<string>(props.book.coverUrl);
+    const [coverSourceFile, setCoverSourceFile] = useState<File>();
+    const [coverSourceURL, setCoverSourceURL] = useState<string>("");
+    const [coverSourceType, setCoverSourceType] = useState<string>(defaultCoverSourceType);
+
     useEffect(
-        ()=> updateBook(props.book),
+        ()=> {
+            updateBook(props.book)
+            setDisplayedCoverImage(props.book.coverUrl);
+            setCoverSourceFile(undefined);
+            setCoverSourceURL("");
+            setCoverSourceType(defaultCoverSourceType)
+        },
         [ props.book ]
     );
     const navigate = useNavigate();
@@ -24,25 +37,80 @@ export default function AddEditBookForm( props: Props ) {
         } );
     }
 
-    function onChangeFcnI( event: ChangeEvent<any> ) {
+    function onChangeInputFcn( event: ChangeEvent<any> ) {
         updateBookValue( event.target.name, event.target.value );
     }
 
-    function saveChanges( event: FormEvent<HTMLFormElement> ) {
+    function onSubmitForm( event: FormEvent<HTMLFormElement> ) {
         event.preventDefault();
-        props.saveBook(book);
+        props.saveBook(book); // TODO: consider selected cover
     }
 
+    function onChangeSelectFcn( event: ChangeEvent<HTMLSelectElement> ) {
+        setCoverSourceType(event.target.value);
+        if (event.target.value==="url" && coverSourceURL)
+            setDisplayedCoverImage(coverSourceURL); // as preview
+        if (event.target.value==="file" && coverSourceFile)
+            updateDisplayedCoverImageFromFile(coverSourceFile);
+    }
+
+    function onChangeCoverURL( event: ChangeEvent<HTMLInputElement> ) {
+        setCoverSourceURL(event.target.value);
+        setDisplayedCoverImage(event.target.value); // as preview
+    }
+
+    function onChangeCoverFile( event: ChangeEvent<HTMLInputElement> ) {
+        if (event?.target?.files && event.target.files.length>=1) {
+            const selectedFile: File = event.target.files[0];
+            setCoverSourceFile(selectedFile);
+            updateDisplayedCoverImageFromFile(selectedFile);
+        }
+    }
+
+    function updateDisplayedCoverImageFromFile(selectedFile: File) {
+        makeDataURL(selectedFile, dataURL => {
+            setDisplayedCoverImage(dataURL);
+        }); // as preview
+    }
+
+    function makeDataURL(file: File, processDataURL: (dataURL: string) => void): void {
+        file.arrayBuffer()
+            .then(value => {
+                const uint8Array = new Uint8Array(value);
+                const numArr = Array.from(uint8Array);
+                const base64String = btoa( String.fromCharCode.apply(null, numArr) );
+                processDataURL(
+                    "data:"+file.type+";base64,"+base64String
+                );
+            })
+            .catch(error => {
+                console.error("Error while converting a File into a DataURL", error);
+            })
+    }
+
+    let cssClassFileInput = coverSourceType==="file" ? "" : "hidden";
+    let cssClassUrlInput = coverSourceType==="url"  ? "" : "hidden";
     return (
         <>
-            <form onSubmit={saveChanges}>
-                <label htmlFor="fld_title"       >Title       :</label><input    id="fld_title"       name="title"       value={book.title      } onChange={onChangeFcnI}/>
-                <label htmlFor="fld_author"      >Author      :</label><input    id="fld_author"      name="author"      value={book.author     } onChange={onChangeFcnI}/>
-                <label htmlFor="fld_description" >Description :</label><textarea id="fld_description" name="description" value={book.description} onChange={onChangeFcnI}/>
-                <label htmlFor="fld_publisher"   >Publisher   :</label><input    id="fld_publisher"   name="publisher"   value={book.publisher  } onChange={onChangeFcnI}/>
-                <label htmlFor="fld_isbn"        >ISBN        :</label><input    id="fld_isbn"        name="isbn"        value={book.isbn       } onChange={onChangeFcnI}/>
-                <label htmlFor="fld_coverUrl"    >Cover URL   :</label><input    id="fld_coverUrl"    name="coverUrl"    value={book.coverUrl   } onChange={onChangeFcnI}/>
-                {book.coverUrl && <img alt="Cover Image" src={book.coverUrl}/>}
+            <form onSubmit={onSubmitForm}>
+                <label htmlFor="fld_title"       >Title       :</label><input    id="fld_title"       name="title"       value={book.title      } onChange={onChangeInputFcn}/>
+                <label htmlFor="fld_author"      >Author      :</label><input    id="fld_author"      name="author"      value={book.author     } onChange={onChangeInputFcn}/>
+                <label htmlFor="fld_description" >Description :</label><textarea id="fld_description" name="description" value={book.description} onChange={onChangeInputFcn}/>
+                <label htmlFor="fld_publisher"   >Publisher   :</label><input    id="fld_publisher"   name="publisher"   value={book.publisher  } onChange={onChangeInputFcn}/>
+                <label htmlFor="fld_isbn"        >ISBN        :</label><input    id="fld_isbn"        name="isbn"        value={book.isbn       } onChange={onChangeInputFcn}/>
+                {/*
+                <label htmlFor="fld_coverUrl"    >Cover URL   :</label><input    id="fld_coverUrl"    name="coverUrl"    value={book.coverUrl   } onChange={onChangeInputFcn}/>
+*/}
+                <label>Cover :</label>
+                <div>
+                    <select value={coverSourceType} onChange={onChangeSelectFcn}>
+                        <option value="url">from URL</option>
+                        <option value="file">from local file</option>
+                    </select>
+                    <input className={cssClassFileInput} type="file" onChange={onChangeCoverFile}/>
+                    <input className={cssClassUrlInput } type="text" onChange={onChangeCoverURL } value={coverSourceURL}/>
+                </div>
+                {displayedCoverImage && <img alt="Cover Image" src={displayedCoverImage}/>}
                 <div>
                     <button>{props.saveButtonTitle}</button>
                     <button type="button" onClick={() => navigate("/")}>Cancel</button>

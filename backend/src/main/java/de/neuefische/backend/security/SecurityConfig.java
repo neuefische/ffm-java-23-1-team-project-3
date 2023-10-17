22a6a9e546c51e97dec1a5024c8f7116fa11880d
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2023-10-17 11:14:48 +0200

    feat: added status 401 when user is not logged in

--- backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
@@ -3,11 +3,13 @@ package de.neuefische.backend.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -31,7 +33,13 @@ public class SecurityConfig {
 
 				.oauth2Login(withDefaults())
 
+				.logout(logout -> logout
+						.logoutUrl("/api/logout")
+						.logoutSuccessHandler((request, response, authentication) ->
+								response.setStatus(200)))
 
+				.exceptionHandling(exceptionHandlingConfigurer -> exceptionHandlingConfigurer
+						.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
 		;
 
 		return http.build();
